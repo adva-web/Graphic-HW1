23d4130fea52cfce737d1dbe250d6bbd24350e00
noamblau <40488203+noamblau@users.noreply.github.com>
2023-04-22 10:59:54 +0300

    update

--- poisson_blending.py
+++ poisson_blending.py
@@ -139,7 +139,7 @@ def poisson_blend(im_src, im_tgt, im_mask, center):
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('--src_path', type=str, default='./data/imgs/banana2.jpg', help='image file path')
-    parser.add_argument('--mask_path', type=str, default='./data/seg_GT/banana1.bmp', help='mask file path')
+    parser.add_argument('--mask_path', type=str, default='./data/seg_GT/banana2.bmp', help='mask file path')
     parser.add_argument('--tgt_path', type=str, default='./data/bg/table.jpg', help='mask file path')
     return parser.parse_args()
 
